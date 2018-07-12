537c613abf00b04f33e43dda68ca17bcdc705030
黃凱麟 <kuroleo@icloud.com>
2018-07-12 14:31:43 +0800

    Initial Commit

---
+++ homework1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by 黃凱麟 on 2018/7/12.
+//  Copyright © 2018年 shinxing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
